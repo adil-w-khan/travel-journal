138bff9697d68793a4d3801a585f1ffa8eb46276
Adil Khan <Adil@Adils-MacBook-Pro.local>
2024-08-26 18:07:03 -0400

    finish project

--- travel-journal/src/components/Post.js
+++ travel-journal/src/components/Post.js
@@ -1,11 +1,21 @@
 import React from "react";
+import pindrop from "../pindrop.png"
 
 export default function Post(props){
   return(
     <div className="post-div">
-      <img src={props.imageUrl} />
+      
+      <img src={props.imageUrl} className="post-image"/>
+      
       <div className="post-details">
-        <p>{props.title}</p>
+        <div className="top-row">
+          <img src={pindrop} className="pindrop-img"/>
+          <p className="location">{props.location}</p>
+          <a href={props.googleMapsUrl} className="google-map-url" target="_blank">View on Google Maps</a>
+        </div>
+        <p className="title">{props.title}</p>
+        <p className="start-end-dates">{props.startDate} - {props.endDate}</p>
+        <p className="description">{props.description}</p>
       </div>
     </div>
   )
--- travel-journal/src/data.js
+++ travel-journal/src/data.js
@@ -1,13 +1,65 @@
 import {nanoid} from "nanoid"
+import globe from "./globe.png"
+
 export default [
   {
+    key: nanoid(),
+    title: "Chicago",
+    location: "USA",
+    googleMapsUrl: "https://maps.app.goo.gl/MDi2eaVV9k3eEtnt5",
+    startDate: "23 Jul, 2024",
+    endDate: "27 Jul, 2024",
+    description: "Chicago, known as the Windy City, is a dynamic metropolis celebrated for its iconic skyline, deep-dish pizza, and rich cultural scene. With world-class museums, diverse neighborhoods, and a vibrant lakefront, it's a city that offers a blend of history, art, and modern urban life.",
+    imageUrl: "https://erepublic.brightspotcdn.com/dims4/default/13ede82/2147483647/strip/false/crop/1000x664+0+0/resize/1000x664!/quality/90/?url=http%3A%2F%2Ferepublic-brightspot.s3.us-west-2.amazonaws.com%2Fb5%2F48%2F0fd02b1c4df7acadea1470bd37c8%2Fshutterstock-84639565.jpg"
+  },
+  {
+    key: nanoid(),
+    title: "Niagara Falls",
+    location: "Canada",
+    googleMapsUrl: "https://maps.app.goo.gl/yBkgs1A5M55KqEG88",
+    startDate: "30 Jul, 2023",
+    endDate: "02 Aug, 2023",
+    description: "Niagara Falls is famous for its breathtaking waterfalls, offering visitors stunning views and thrilling experiences like boat tours and observation decks. The surrounding area features vibrant attractions, parks, and wineries, making it a must-visit destination for natural beauty and adventure.",
+    imageUrl: "https://cdn.aarp.net/content/dam/aarp/travel/destinations/2021/08/1140-journey-behind-the-falls.jpg"
+  },
+  {
+    key: nanoid(),
+    title: "Lake George",
+    location: "New York",
+    googleMapsUrl: "https://maps.app.goo.gl/Cd2xeuUfccNs6sDo6",
+    startDate: "10 Aug, 2022",
+    endDate: "14 Aug, 2022",
+    description: "Lake George, nestled in the Adirondack Mountains of New York, is a picturesque destination known for its crystal-clear waters and scenic beauty. Offering a range of outdoor activities, from boating and hiking to historic sites, it's an ideal spot for nature lovers and vacationers seeking tranquility.",
+    imageUrl: "https://cannaprovisions.com/wp-content/uploads/2023/03/iStock-1337872215.jpg"
+  },
+  {
+    key: nanoid(),
+    title: "Orlando",
+    location: "Florida",
+    googleMapsUrl: "https://maps.app.goo.gl/vkMCKuBT2MgQkf3T7",
+    startDate: "25 Dec, 2021",
+    endDate: "01 Jan, 2022",
+    description: "Orlando, Florida, is a world-renowned destination famous for its theme parks, including Walt Disney World and Universal Studios. Beyond its attractions, the city offers a mix of lively entertainment, outdoor adventures, and a warm, welcoming atmosphere.",
+    imageUrl: "https://media.istockphoto.com/id/538010989/photo/orlando-florida-skyline.jpg?s=612x612&w=0&k=20&c=p08rABnBPW8oEwu2RtzYXUdQaL9s2fM_mlTGZBcdYHs="
+  },
+  {
       key: nanoid(),
-      title: "Mount Fuji",
-      location: "Japan",
-      googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-      startDate: "12 Jan, 2021",
-      endDate: "24 Jan, 2021",
-      description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-      imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
-  }
+      title: "Karachi",
+      location: "Pakistan",
+      googleMapsUrl: "https://maps.app.goo.gl/jKxHpQyHt4rvHFu27",
+      startDate: "09 Jul, 2021",
+      endDate: "29 Aug, 2021",
+      description: "Karachi, Pakistan's largest city, is a bustling coastal metropolis known for its diverse culture, historic landmarks, and vibrant street life. As a major economic hub, it offers a rich blend of traditional markets, modern developments, and scenic seaside views.",
+      imageUrl: "https://st4.depositphotos.com/7105460/24298/i/450/depositphotos_242989180-stock-photo-beautiful-view-bahadurabad-chorangi-karachi.jpg"
+  },
+  {
+    key: nanoid(),
+    title: "Dubai",
+    location: "UAE",
+    googleMapsUrl: "https://maps.app.goo.gl/SW97yQYR3eMvczwFA",
+    startDate: "04 Jul, 2021",
+    endDate: "09 Jul, 2021",
+    description: "Dubai is a vibrant metropolis known for its stunning modern architecture, luxury shopping, and lively nightlife. It blends traditional Arabian culture with cutting-edge innovation, making it a unique destination for travelers.",
+    imageUrl: "https://w0.peakpx.com/wallpaper/393/408/HD-wallpaper-burj-khalifa-city-dubai-night-life-thumbnail.jpg"
+}
 ]
